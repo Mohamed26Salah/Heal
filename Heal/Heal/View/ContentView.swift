259c78b1c2d4ce78a984d8ccd86d8ffc4aad3962
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2023-09-18 21:48:22 +0300

    Added SplashScreen

---
+++ Heal/Heal/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Heal
+//
+//  Created by Mohamed Salah on 18/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Heal/Heal/View/HealApp.swift
@@ -0,0 +1,18 @@
+//
+//  HealApp.swift
+//  Heal
+//
+//  Created by Mohamed Salah on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            SplashScreen()
+        }
+    }
+}
